658634f9ae78017700855a2549901c13920f8235
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-12-29 23:31:52 +0600

    Add Main component and integrate JSON server for question data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, status: "ready", questions: action.payload };
+    case "loading":
+      return { ...state, status: "loading" };
+    case "error":
+      return { ...state, status: "error" };
+    case "ready":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      return new Error("Invalid action type");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div>
-      <DateCounter />
+      <Header />
+      <Main>
+        <p> 1/15 </p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
