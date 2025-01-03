ab91056e5a2e63fd4ff7974348a8d87f28b16cf1
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-03 15:22:54 +0600

    Add answer tracking and points calculation in quiz; update Options and Question components for new functionality

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // loading, error, ready
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -28,13 +30,23 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const currentQuestion = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === currentQuestion.correctOption
+            ? state.points + currentQuestion.points
+            : state.points,
+      };
     default:
       return new Error("Invalid action type");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -56,7 +68,13 @@ export default function App() {
           <StartScreen numberQuestions={numberQuestions} dispatch={dispatch} />
         )}
 
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
